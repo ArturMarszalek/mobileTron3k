fe87f103e5c45c3f7a19d1a321442012b805644e
wilku <marcin.wilczynski@happyteam.io>
2017-09-28 13:04:49 +0100

    Add lacking references

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
