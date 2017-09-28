5fa088cb55968504fb9162f2b2c48759032aca74
Mark Willmer <mark@storefeeder.com>
2017-09-28 12:04:16 +0100

    Add API

---
+++ API/mobileTron3kApi/Controllers/DashboardController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MobileTron3kApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DashboardController : Controller
+    {
+        public readonly IConfiguration _configuration;
+
+        public DashboardController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{accountId}")]
+        public async Task<IEnumerable<Models.Summary>> Get(int accountId)
+        {
+            var connectionString = ConfigurationExtensions.GetConnectionString(_configuration, "Main");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return (await connection
+                    .QueryAsync<Models.Summary>(@"
+                        select
+                            OrderDate,
+                            NumberOfOrders = count(*),
+                            ValueOfOrders = sum(OrderTotal)
+                        from
+                            (
+                                select
+                                    AccountID,
+                                    OrderDate = convert(date, OrderDate at time zone 'UTC' at time zone 'GMT Standard Time'),
+                                    OrderTotal,
+                                    IsDeleted
+                                from
+                                    [Order]
+                                where
+                                    AccountID = @AccountID
+                                    and IsDeleted = 0
+                                    and OrderDate >= convert(date, dateadd(day, -31, getutcdate()))
+                            ) a
+                        group by
+                            a.OrderDate
+                        order by
+                            a.OrderDate desc
+                        ",
+                        new { AccountID = accountId }))
+                    .AsList();
+            }
+        }
+    }
+}
---
+++ API/mobileTron3kApi/Models/Summary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MobileTron3kApi.Models
+{
+    public class Summary
+    {
+        public DateTime OrderDate { get; set; }
+        public int NumberOfOrders { get; set; }
+        public decimal ValueOfOrders { get; set; }
+    }
+}
